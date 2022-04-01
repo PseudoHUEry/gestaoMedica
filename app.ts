512f3122adb208273c41d43bd35adc9059f947e8
Harry Raddatz <85199461+PseudoHUEry@users.noreply.github.com>
2022-04-01 19:09:09 -0300

    Feat Dockerfile, Dockercompose, amqp e ajustes para build

--- app.ts
+++ app.ts
@@ -1,11 +1,13 @@
 import { startServer } from "./src/infraestructure";
 import { startDatabase } from "./src/database/sequelize";
+import AmqpStarter from "./src/config/AmqpStarter";
 import moment from "moment-timezone";
 import env from './src/enviroment/env'
 moment.tz.setDefault('America/Sao_Paulo')
 try {
     const app = startServer(env.port)
     const database = startDatabase()
+    // const amqp = AmqpStarter()
 } catch (e) {
     console.log(e)
 }
--- src/database/sequelize/index.ts
+++ src/database/sequelize/index.ts
@@ -1,13 +1,19 @@
 import { Sequelize } from 'sequelize'
 import env from '../../enviroment/env'
 const database = new Sequelize(env.databaseName, env.databaseUser, env.databasePass, {
-    dialect: env.databaseDialect
+    dialect: env.databaseDialect,
+    port: Number(env.databasePort)
 })
 
 
 export default database
 
-export const startDatabase = async() =>{
-    await database.sync()
-    return true
+export const startDatabase = async () => {
+    try {
+        await database.sync()
+        return true
+    } catch (e) {
+        console.log(e)
+        return false
+    }
 }
